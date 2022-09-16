f7bd1eb25287a76cf488d7f212b9a5da8a95399d
DuniaMahdi88 <dunia.mahdi@hotmail.com>
2022-09-16 09:50:53 +0200

    Kommentarer i CalcTrackAndField

--- src/main/java/common/CalcTrackAndField.java
+++ src/main/java/common/CalcTrackAndField.java
@@ -1,16 +1,21 @@
 package common;
 
+//Friidrott, Beräknar poäng
 public class CalcTrackAndField {
 
+	//Definierar tillstånden/attributen
 	private int result;
 
-	// Calculates height and distance for field discipline.
+	//Beräknar höjd och distans för fältdisciplin.
+	//Definierar metoderna (egenskaperna)
 	public int calculateField(double A, double B, double C, double distance) {
+		//Math.pow accepterar två parametrar och returnerar värdet för den första parametern upphöjd till den andra parametern.
 		result = (int) (A * Math.pow((distance - B), C));
 		return result;
 	}
 
-	// Calculates height and distance for track discipline.
+	//Beräknar höjd och distans för bandisciplin.
+	//Definierar metoderna (egenskaperna)
 	public int calculateTrack(double A, double B, double C, double distance) {
 		result = (int) (A * Math.pow((B - distance), C));
 		return result;
