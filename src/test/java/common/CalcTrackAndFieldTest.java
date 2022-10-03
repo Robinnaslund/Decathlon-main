33b1c6cb17abf3b37a120351eb9bd7281191731f
DuniaMahdi88 <dunia.mahdi@hotmail.com>
2022-10-03 12:30:54 +0200

    testklassen CalcTrackAndField

---
+++ src/test/java/common/CalcTrackAndFieldTest.java
@@ -0,0 +1,27 @@
+package common;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class CalcTrackAndFieldTest {
+
+    @Test
+    public void addCompetitior()
+    {
+        //Arrange
+
+        //Pet dog = new Pet (”Karo”);
+
+        //Act
+
+        //String petName = dog.getName();
+
+
+        //Assert
+
+        //assertEquals (expected “Karo”, petName);
+
+    }
+}
