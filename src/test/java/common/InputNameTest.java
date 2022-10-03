ce81f572dbe93270af571a1c5f34067c3a554734
Millrud <camilla.amadian@gmail.com>
2022-10-03 12:07:59 +0200

    Test class InputNameTest added

---
+++ src/test/java/common/InputNameTest.java
@@ -0,0 +1,29 @@
+package common;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+class InputNameTest {
+
+    @Test
+    public void addCompetitior()
+    {
+        //Arrange
+
+        //Pet dog = new Pet (”Karo”);
+
+        //Act
+
+        //String petName = dog.getName();
+
+
+        //Assert
+
+        //assertEquals (expected “Karo”, petName);
+
+    }
+
+}
+
+
+
