376be79a595beec3068dc43562d7a85535b07adf
robinnaslund <robin.naslund96@gmail.com>
2022-10-07 12:38:12 +0200

    input result test

--- src/test/java/common/InputResultTest.java
+++ src/test/java/common/InputResultTest.java
@@ -9,23 +9,75 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class InputResultTest {
 
-   /* @Test
-    public void addCompetitorName() {
+    @Test
+    public void tesinputResult() {
 
         //Arrange
-        ByteArrayInputStream in = new ByteArrayInputStream("11.0".getBytes());
-        System.setIn(in);
 
         InputResult myInput = new InputResult();
 
         //Act
-        myInput.enterResult();
+        myInput.enterResult(20);
         double actual = myInput.returnResult();
 
         //Assert
-        double expected = 11.0;
+        double expected = 20.0;
         Assertions.assertEquals(expected, actual);
-    }*/
 
+
+    }
+
+    @Test
+    public void tesinputInCorrectResult() {
+
+        //Arrange
+
+
+        InputResult myInput = new InputResult();
+
+        //Act
+        myInput.enterResult(8.0);
+        double actual = myInput.returnResult();
+
+        //Assert
+        double expected = 8.0;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void tesinputResultZero() {
+
+        //Arrange
+
+        InputResult myInput = new InputResult();
+
+        //Act
+        //myInput.enterResult(0);
+        //myInput.validateResultInput("f");
+        boolean actual = myInput.validateResultInput("f");
+
+        //Assert
+
+        Assertions.assertEquals(false, actual);
+    }
+
+    @Test
+    public void testinputResult100() {
+
+        //Arrange
+
+        InputResult myInput = new InputResult();
+
+        //Act
+        //myInput.enterResult(100);
+       // myInput.validateResultInput("100");
+        boolean actual = myInput.validateResultInput("100");
+
+        //Assert
+        Assertions.assertEquals(false, actual);
+    }
 
 }
+
+
+
